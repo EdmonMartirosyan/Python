e892d847cc0d3ac790e29597d1e35c69fe491cb2
EdgarMartirosyann <emartirosyan096@gmail.com>
2020-10-16 20:12:28 +0400

    tnayin

--- homework_.py
+++ homework_.py
@@ -1,9 +1,8 @@
 #1
 #for i in range(0, 101):
     #for j in range(0, 101):
-        #for k in range(0, 101):
-            #if k != j != i and k**j == i and j**k == i:
-                #print(k, j, i)
+        #if j != i and i**j == j**i:
+            #print(j, i)
 #2
 #for i in range(1,5):
     #print(i*f"{i}")
