ab9ee8ea1db6631f5b5c2e8d6530cfa77e3cdb33
EdgarMartirosyann <72929495+EdgarMartirosyann@users.noreply.github.com>
2020-11-05 23:56:54 +0400

    Update homework1_7.py

--- homework1_7.py
+++ homework1_7.py
@@ -1,18 +1,16 @@
 #1
 def answer_queries(k, *query_counts):
     count = 0
-    number = 1
+    number = 0
     for i in range(len(query_counts)):
         if query_counts[i] + count >= k:
             count = query_counts[i] + count - k
             number += 1
         else:
-            return number
-    while count > 0:
-        count -= k
-        number += 1
-    return number
+            return number+1
+    if count > 0:
+        number += count // k 
+    return number+1
 
 
-print(answer_queries(1, 100))
-
+print(answer_queries(5, 10, 5, 5, 3, 2, 1))
