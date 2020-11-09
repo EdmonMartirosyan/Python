384f16298b88545275849deba9ff78d8c70b6791
EdgarMartirosyann <72929495+EdgarMartirosyann@users.noreply.github.com>
2020-11-09 23:54:33 +0400

    Update tnayin.py

--- tnayin.py
+++ tnayin.py
@@ -1,9 +1,11 @@
-n = input()
-a = []
-d = {}
-while n != "End":
-    a.append(int(n))
-    n = input()
-for el in a:
-    d[el] = a.count(el)
-print(d)
+def buildPalindrome(string):
+    for el in string:
+        if string[-(string.find(el)+1)] != el:
+            if string.find(el) == 0:
+                string = string[:] + el
+            else:
+                string = string[:-(string.find(el))] + el + string[-(string.find(el)):]
+    return string
+
+
+print(buildPalindrome("abcdc"))
