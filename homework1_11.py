d1cd517fd1d5f22707f9a88e3dfb4f6e60991e9b
EdgarMartirosyann <72929495+EdgarMartirosyann@users.noreply.github.com>
2020-11-16 23:50:43 +0400

    Update homework1_11.py

--- homework1_11.py
+++ homework1_11.py
@@ -1,23 +1,62 @@
 #1
-import copy
-def rotateImage(a):
-    new_a = copy.deepcopy(a)
-    t = 1
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            new_a[j][len(new_a)-t] = a[i][j]
-        t += 1
-    return new_a
+# import copy
+# def rotateImage(a):
+#     new_a = copy.deepcopy(a)
+#     t = 1
+#     for i in range(len(a)):
+#         for j in range(len(a[i])):
+#             new_a[j][len(new_a)-t] = a[i][j]
+#         t += 1
+#     return new_a
+#
+#
+# ex1 = [[1, 2, 3],
+#        [4, 5, 6],
+#        [7, 8, 9]]
+# ex2 = [[10, 9, 6, 3, 7],
+#        [6, 10, 2, 9, 7],
+#        [7, 6, 3, 8, 2],
+#        [8, 9, 7, 9, 9],
+#        [6, 8, 6, 8, 2]]
+#
+#
+# print(rotateImage(ex2))
 
 
-ex1 = [[1, 2, 3],
-       [4, 5, 6],
-       [7, 8, 9]]
-ex2 = [[10, 9, 6, 3, 7],
-       [6, 10, 2, 9, 7],
-       [7, 6, 3, 8, 2],
-       [8, 9, 7, 9, 9],
-       [6, 8, 6, 8, 2]]
+#2
+def f(number):
+    lst = []
+    c = 2
+    while number != 1:
+        if number % c == 0:
+            lst.append(c)
+            number /= c
+        else:
+            c += 1
+    return lst
 
 
-print(rotateImage(ex2))
+def digitsProduct(product):
+    if len(str(product)) == 1:
+        return int('1' + str(product))
+    number = ''
+    lst = f(product)
+    if len(lst) == 1:
+        return -1
+    number += str(lst[0])
+    flag = True
+    for i in range(2, len(lst)):
+        if flag:
+            if len(str(lst[i] * lst[i - 1])) == 1:
+                number += str(lst[i] * lst[i - 1])
+                flag = False
+            else:
+                number += str(lst[i])
+                number += str(lst[i - 1])
+        else:
+            flag = True
+    list(number).sort()
+    return int(number)
+
+
+print(digitsProduct(450))
