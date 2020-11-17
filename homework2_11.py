fea4cb9074fa8c39b1d9ce7afcacdd01009c6a37
EdgarMartirosyann <72929495+EdgarMartirosyann@users.noreply.github.com>
2020-11-17 18:07:31 +0400

    Update homework2_11.py

--- homework2_11.py
+++ homework2_11.py
@@ -1,11 +1,22 @@
 #1
-def number_of_digits(n):
-    a = 10
-    count = 1
-    while a <= n:
-        a *= 10
-        count +=1
-    return count
+# def number_of_digits(n):
+#     a = 10
+#     count = 1
+#     while a <= n:
+#         a *= 10
+#         count +=1
+#     return count
+# 
+# 
+# print(number_of_digits(432))
+
+#2
+def sum_numbers(lst, n):
+    set_ = set()
+    for i in range(len(lst)):
+        if n - lst[i] in lst:
+            set_.add((lst[i], n - lst[i]))
+    return
 
 
-print(number_of_digits(432))
+print(sum_numbers([5, 2, 2, 5, 3],8))
