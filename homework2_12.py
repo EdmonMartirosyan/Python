02a41b26ffdc4bf052e61163f4b8739e2dac9626
EdgarMartirosyann <72929495+EdgarMartirosyann@users.noreply.github.com>
2020-11-19 22:40:26 +0400

    Update homework2_12.py

--- homework2_12.py
+++ homework2_12.py
@@ -14,3 +14,17 @@
 #
 # print_numbers(9)
 
+#2
+def lucky_number(lst):
+    if lst[0] == len(lucky_number.list_1):
+        return lst[0]
+    else:
+        new_list = lst[lst[0]-len(lucky_number.list_1):]
+        if len(new_list) == 0:
+            return -1
+        lucky_number.list_1 += lst[:lst[0]-len(lucky_number.list_1)]
+        return lucky_number(new_list)
+
+
+lucky_number.list_1 = []
+print(lucky_number([1, 2, 2, 5, 7]))
